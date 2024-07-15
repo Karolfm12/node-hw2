26322ca21d66e3c75df29b0b6712caa2b7010344
Karolfm12 <karolfm12@onet.eu>
2024-07-15 20:13:41 +0200

    done

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://karolfm12:talking12@cluster0.e0s0opq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connection successful");
   } catch (error) {
     console.error("Database connection error:", error);
